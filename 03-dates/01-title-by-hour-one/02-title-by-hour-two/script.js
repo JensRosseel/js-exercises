c1e457a6cec4b05c76f577a515683fbb3c22a45b
JensRosseel <slaythehorizon.games@gmail.com>
2021-06-14 13:33:54 +0200

    Add additonal solutions

---
+++ 03-dates/01-title-by-hour-one/02-title-by-hour-two/script.js
@@ -0,0 +1,27 @@
+/* becode/javascript
+ *
+ * /04-dates/02-title-by-hour-two/script.js - 4.2: texte en fonction de l'heure (2)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+
+    var d = new Date();
+    hour = d.getHours();
+    minutes = d.getMinutes();
+
+    if(hour == 17 && minutes < 30 || hour == 17){
+        document.getElementById("target").innerHTML = "Hello!";
+    }
+    else{
+        document.getElementById("target").innerHTML = "Good Evening!";
+    }
+
+})();
